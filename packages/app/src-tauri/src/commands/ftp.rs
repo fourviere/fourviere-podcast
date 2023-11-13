435fd508c762c3fdcf1dbb8a54c529032adbcf1c
Flavio Bizzarri <33379291+newfla@users.noreply.github.com>
2023-11-13 21:09:51 +0100

    Unit test fs, ftp, network functions (#32)
    * Unit test fs, ftp, network functions
    
    * comments clean-up
    
    * test_file moved to utils/test.rs
    
    * default ftp connection to passive mode

--- packages/app/src-tauri/src/commands/ftp.rs
+++ packages/app/src-tauri/src/commands/ftp.rs
@@ -31,7 +31,16 @@ pub async fn ftp_upload(payload: Payload) -> Result<String, String> {
         return Err("Failed to login to server: {}".to_string());
     }
 
-    ftp_stream.set_mode(Mode::ExtendedPassive);
+    // As default set the FTP connection to passive mode
+    ftp_stream.set_mode(Mode::Passive);
+
+    // https://www.iana.org/assignments/ftp-commands-extensions/ftp-commands-extensions.xhtml
+    if ftp_stream
+        .feat()
+        .is_ok_and(|opts| opts.get("EPSV").is_some())
+    {
+        ftp_stream.set_mode(Mode::ExtendedPassive);
+    }
 
     if let Some(path) = &payload.path {
         let change_path = ftp_stream.cwd(path);
@@ -57,6 +66,7 @@ pub async fn ftp_upload(payload: Payload) -> Result<String, String> {
 
     let mut reader = BufReader::new(file.unwrap());
     let res = ftp_stream.put_file(format!("{}.{}", &payload.file_name, &ext), &mut reader);
+
     if let Err(_) = res {
         return Err("Failed to upload file".to_string());
     }
@@ -76,4 +86,157 @@ pub async fn ftp_upload(payload: Payload) -> Result<String, String> {
         payload.file_name,
         ext
     ))
+}
+
+#[cfg(test)]
+mod test {
+    use std::{sync::Arc, time::Duration};
+
+    use async_trait::async_trait;
+    use libunftp::{
+        auth::{AuthenticationError, Authenticator, Credentials, DefaultUser},
+        ServerError,
+    };
+    use tokio::time::sleep;
+
+    use crate::{
+        commands::ftp::{ftp_upload, Payload},
+        test_file,
+    };
+
+    #[derive(Debug)]
+    struct TestAuthenticator;
+
+    #[async_trait]
+    impl Authenticator<DefaultUser> for TestAuthenticator {
+        async fn authenticate(
+            &self,
+            username: &str,
+            creds: &Credentials,
+        ) -> Result<DefaultUser, AuthenticationError> {
+            match username {
+                "ForuviereTestUser" => match creds.password.as_deref() {
+                    Some("StealThisUselessPassword") => Ok(DefaultUser),
+                    _ => Err(AuthenticationError::BadPassword),
+                },
+                _ => Err(AuthenticationError::BadUser),
+            }
+        }
+    }
+
+    async fn ftp_server(port: u16) -> Result<(), ServerError> {
+        let server = libunftp::Server::with_authenticator(
+            Box::new(move || unftp_sbe_fs::Filesystem::new("/tmp")),
+            Arc::new(TestAuthenticator {}),
+        )
+        .passive_ports(50000..65535);
+
+        server.listen(format!("127.0.0.1:{}", port)).await
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_ftp_upload_ok() {
+        let port = 2121;
+        let payload = Payload {
+            host: "localhost".to_string(),
+            port: port,
+            user: "ForuviereTestUser".to_string(),
+            password: "StealThisUselessPassword".to_string(),
+            local_path: test_file!("gitbar.xml").to_string(),
+            path: None,
+            file_name: "gitbar".to_string(),
+            http_host: "localhost".to_string(),
+            https: false,
+        };
+
+        let handle = tokio::spawn(async move {
+            assert!(ftp_server(port).await.is_ok());
+        });
+
+        // Hopefully the server is up =D
+        sleep(Duration::from_secs(2)).await;
+        assert!(ftp_upload(payload).await.is_ok());
+        handle.abort();
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_ftp_upload_err_host() {
+        let port = 2122;
+        let payload = Payload {
+            host: "localhosts".to_string(),
+            port: port,
+            user: "ForuviereTestUser".to_string(),
+            password: "StealThisUselessPassword".to_string(),
+            local_path: test_file!("gitbar.xml").to_string(),
+            path: None,
+            file_name: "gitbar".to_string(),
+            http_host: "localhosts".to_string(),
+            https: false,
+        };
+
+        let handle = tokio::spawn(async move {
+            assert!(ftp_server(port).await.is_ok());
+        });
+
+        // Hopefully the server is up =D
+        sleep(Duration::from_secs(2)).await;
+        assert!(ftp_upload(payload)
+            .await
+            .is_err_and(|err| err == "Failed to connect to server"));
+        handle.abort();
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_ftp_upload_err_user() {
+        let port = 2123;
+        let payload = Payload {
+            host: "localhost".to_string(),
+            port: port,
+            user: "NotAValidUserName".to_string(),
+            password: "StealThisUselessPassword".to_string(),
+            local_path: test_file!("gitbar.xml").to_string(),
+            path: None,
+            file_name: "gitbar".to_string(),
+            http_host: "localhost".to_string(),
+            https: false,
+        };
+
+        let handle = tokio::spawn(async move {
+            assert!(ftp_server(port).await.is_ok());
+        });
+
+        // Hopefully the server is up =D
+        sleep(Duration::from_secs(2)).await;
+        assert!(ftp_upload(payload)
+            .await
+            .is_err_and(|err| err == "Failed to login to server: {}"));
+        handle.abort();
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_ftp_upload_err_local_path() {
+        let port = 2124;
+        let payload = Payload {
+            host: "localhost".to_string(),
+            port: port,
+            user: "ForuviereTestUser".to_string(),
+            password: "StealThisUselessPassword".to_string(),
+            local_path: test_file!("gitbar.rss").to_string(),
+            path: None,
+            file_name: "gitbar".to_string(),
+            http_host: "localhost".to_string(),
+            https: false,
+        };
+
+        let handle = tokio::spawn(async move {
+            assert!(ftp_server(port).await.is_ok());
+        });
+
+        // Hopefully the server is up =D
+        sleep(Duration::from_secs(2)).await;
+        assert!(ftp_upload(payload)
+            .await
+            .is_err_and(|err| err == "Failed to open file"));
+        handle.abort();
+    }
 }
