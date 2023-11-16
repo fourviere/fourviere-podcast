542954ad106b5e078585d98afcae339db853fb2d
flavio <newfla@hotmail.it>
2023-11-16 11:48:30 +0100

    rename read_file to read_text_file

--- packages/app/src-tauri/src/commands/fs.rs
+++ packages/app/src-tauri/src/commands/fs.rs
@@ -1,7 +1,7 @@
 use tokio::fs::read_to_string;
 
 #[tauri::command]
-pub async fn read_file(path: &str) -> Result<String, String> {
+pub async fn read_text_file(path: &str) -> Result<String, String> {
     read_to_string(path).await.map_err(|err| err.to_string())
 }
 
@@ -9,11 +9,11 @@ pub async fn read_file(path: &str) -> Result<String, String> {
 mod test {
     use rss::Channel;
 
-    use crate::{commands::fs::read_file, test_file};
+    use crate::{commands::fs::read_text_file, test_file};
 
     #[tokio::test]
-    async fn test_read_file_ok() {
-        let feed = read_file(test_file!("gitbar.xml")).await;
+    async fn test_read_text_file_ok() {
+        let feed = read_text_file(test_file!("gitbar.xml")).await;
         assert!(feed.is_ok());
         let channel = Channel::read_from(feed.unwrap().as_bytes());
         assert!(channel.is_ok());
@@ -22,8 +22,8 @@ mod test {
     }
 
     #[tokio::test]
-    async fn test_read_file_error() {
-        let feed = read_file(test_file!("gitbar.rss")).await;
+    async fn test_read_text_file_error() {
+        let feed = read_text_file(test_file!("gitbar.rss")).await;
         assert!(feed.is_err());
     }
 }
