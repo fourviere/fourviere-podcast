e6140579c853b62c1493402f861f1f8a0645e7f6
flavio <newfla@hotmail.it>
2023-11-17 13:19:40 +0100

    Added log const declaration

--- packages/app/src-tauri/src/utils/log.rs
+++ packages/app/src-tauri/src/utils/log.rs
@@ -4,13 +4,17 @@ use tauri_plugin_log::LogTarget;
 #[cfg(not(debug_assertions))]
 const FORCE_DEBUG_LOG_FLAG: &str = "FOURVIERE_FORCE_DEBUG";
 
+const LOG_LEVEL_DEBUG: LevelFilter = LevelFilter::Debug;
+const LOG_TARGETS_DEBUG: [LogTarget; 3] =
+    [LogTarget::Stderr, LogTarget::LogDir, LogTarget::Webview];
+
 pub fn log_settings() -> (LevelFilter, Vec<LogTarget>) {
     (log_level(), log_targets())
 }
 
 #[cfg(debug_assertions)]
 fn log_level() -> LevelFilter {
-    LevelFilter::Debug
+    LOG_LEVEL_DEBUG
 }
 
 #[cfg(not(debug_assertions))]
@@ -24,13 +28,13 @@ fn log_level() -> LevelFilter {
 
 #[cfg(debug_assertions)]
 fn log_targets() -> Vec<LogTarget> {
-    vec![LogTarget::Stderr, LogTarget::Webview]
+    Vec::from(LOG_TARGETS_DEBUG)
 }
 
 #[cfg(not(debug_assertions))]
 fn log_targets() -> Vec<LogTarget> {
     if std::env::var(FORCE_DEBUG_LOG_FLAG).is_ok_and(|val| val.eq_ignore_ascii_case("TRUE")) {
-        vec![LogTarget::Stderr, LogTarget::Webview]
+        Vec::from(LOG_TARGETS_DEBUG)
     } else {
         vec![LogTarget::LogDir]
     }
