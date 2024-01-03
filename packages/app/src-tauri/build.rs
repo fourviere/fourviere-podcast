e25c3e8ae984a04da2c64bf369317f6608fb0c21
Flavio Bizzarri <33379291+newfla@users.noreply.github.com>
2024-01-03 16:37:41 +0100

    fix: Failing FFmpeg download (#135)
    * made windows downlaod more robust
    
    * made linux download more robust

--- packages/app/src-tauri/build.rs
+++ packages/app/src-tauri/build.rs
@@ -14,12 +14,14 @@ const X86_64_UNKNOWN_LINUX_GNU_ARCHIVE: &'static str =
     "https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz";
 const X86_64_UNKNOWN_LINUX_GNU_MD5: &'static str =
     "https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz.md5";
+const X86_64_UNKNOWN_LINUX_GNU_VERSION: &str = "https://johnvansickle.com/ffmpeg";
 
 // WINDOWS SECTION
 const X86_64_PC_WINDOWS_MSVC_ARCHIVE: &'static str =
-    "https://github.com/GyanD/codexffmpeg/releases/download/6.1/ffmpeg-6.1-essentials_build.7z";
+    "https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.7z";
 const X86_64_PC_WINDOWS_MSVC_SHA256: &'static str =
     "https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.7z.sha256";
+const X86_64_PC_WINDOWS_MSVC_VERSION: &str = "https://www.gyan.dev/ffmpeg/builds/release-version";
 
 // APPLE SILICON SECTON
 const AARCH64_APPLE_DARWIN_FFMPEG_ARCHIVE: &'static str =
@@ -86,11 +88,19 @@ fn check_binaries() -> bool {
 
 fn download_x86_64_unknown_linux_gnu() -> Result<()> {
     let target_triple = std::env::var("TARGET")?;
-
-    let tmp_dir = tempdir()?;
-    let tarball_path: std::path::PathBuf = tmp_dir.path().join("ffmpeg-6.1-amd64-static");
-
     let mut decomp: Vec<u8> = Vec::new();
+
+    let version = reqwest::blocking::get(X86_64_UNKNOWN_LINUX_GNU_VERSION)?
+        .text()?
+        .split_once("release:")
+        .unwrap_or_default()
+        .1
+        .trim()
+        .split_once('<')
+        .unwrap_or_default()
+        .0
+        .to_owned();
+
     let md5 = reqwest::blocking::get(X86_64_UNKNOWN_LINUX_GNU_MD5)?
         .text()?
         .split_ascii_whitespace()
@@ -98,7 +108,13 @@ fn download_x86_64_unknown_linux_gnu() -> Result<()> {
         .last()
         .unwrap_or_default()
         .to_owned();
+
     let raw_data = reqwest::blocking::get(X86_64_UNKNOWN_LINUX_GNU_ARCHIVE)?.bytes()?;
+
+    let tmp_dir = tempdir()?;
+    let tarball_path: std::path::PathBuf = tmp_dir
+        .path()
+        .join(format!("ffmpeg-{version}-amd64-static"));
 
     let digest = md5::compute(&raw_data);
     assert_eq!(format!("{:x}", digest), md5);
@@ -120,12 +136,14 @@ fn download_x86_64_unknown_linux_gnu() -> Result<()> {
 fn download_x86_64_pc_windows_msvc() -> Result<()> {
     let target_triple = std::env::var("TARGET")?;
 
-    let tmp_dir = tempdir()?;
-    let windows_binaries_path: std::path::PathBuf =
-        tmp_dir.path().join("ffmpeg-6.1-essentials_build/bin");
-
+    let version = reqwest::blocking::get(X86_64_PC_WINDOWS_MSVC_VERSION)?.text()?;
     let sha256 = reqwest::blocking::get(X86_64_PC_WINDOWS_MSVC_SHA256)?.text()?;
     let raw_data = reqwest::blocking::get(X86_64_PC_WINDOWS_MSVC_ARCHIVE)?.bytes()?;
+
+    let tmp_dir = tempdir()?;
+    let windows_binaries_path: std::path::PathBuf = tmp_dir
+        .path()
+        .join(format!("ffmpeg-{version}-essentials_build/bin"));
 
     let mut hasher = Sha256::new();
     hasher.update(&raw_data);
