a7b925e101ff969dc65507410d785846b862884d
Flavio Bizzarri <33379291+newfla@users.noreply.github.com>
2024-02-27 02:03:17 +0100

    feat: tauri updater  (#201)

---
+++ .github/disable_updater.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const path = "./packages/app/src-tauri/tauri.conf.json";
+fs.readFile(path, "utf8", (error, data) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  let config = JSON.parse(data);
+  config.tauri.updater.active = false;
+  
+  fs.writeFile(path, JSON.stringify(config, null, 2), (error) => {
+    if (error) {
+      console.log('An error has occurred ', error);
+      return;
+    }
+    console.log('Data written successfully to disk');
+  });
+});
