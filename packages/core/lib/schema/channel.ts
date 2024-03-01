9e41e32ad48779e50aad5c6e3b1b4eb96be90f32
Mauro Murru <murru7@gmail.com>
2024-03-01 17:38:42 +0100

    Update language options in General component and add description field to image object in RSSChannelSchema

--- packages/core/lib/schema/channel.ts
+++ packages/core/lib/schema/channel.ts
@@ -3,7 +3,9 @@ import { ItemSchema } from "./item";
 
 // RSS SPECIFICATION https://www.rssboard.org/rss-specification
 const RSSChannelSchema = Type.Object({
+  // MANDATORY
   title: Type.String(),
+  description: Type.String(),
   link: Type.Optional(
     Type.Array(
       Type.Object({
@@ -16,23 +18,48 @@ const RSSChannelSchema = Type.Object({
       }),
     ),
   ),
-  description: Type.String(),
-  language: Type.Optional(Type.String()),
-  copyright: Type.Optional(Type.String()),
-  managingEditor: Type.Optional(Type.String()),
-  webMaster: Type.Optional(Type.String()),
-  pubDate: Type.Optional(Type.String()),
-  lastBuildDate: Type.Optional(Type.String()),
+
+  // OPTIONAL
   category: Type.Optional(Type.Array(Type.String())),
+  copyright: Type.Optional(Type.String()),
   generator: Type.Optional(Type.String()),
-  keywords: Type.Optional(Type.String()),
   image: Type.Optional(
     Type.Object({
       url: Type.String(),
-      title: Type.Optional(Type.String()),
-      link: Type.Optional(Type.String()),
+      title: Type.String(),
+      link: Type.String(),
+      description: Type.Optional(Type.String()),
+      height: Type.Optional(Type.Number()),
+      width: Type.Optional(Type.Number()),
     }),
   ),
+  language: Type.Optional(Type.String()),
+  lastBuildDate: Type.Optional(Type.String()),
+  managingEditor: Type.Optional(Type.String()),
+  pubDate: Type.Optional(Type.String()),
+  skipHours: Type.Optional(
+    Type.Array(
+      Type.Object({
+        hour: Type.Number(),
+      }),
+    ),
+  ),
+  skipDays: Type.Optional(
+    Type.Array(
+      Type.Object({
+        day: Type.Union([
+          Type.Literal("Monday"),
+          Type.Literal("Tuesday"),
+          Type.Literal("Wednesday"),
+          Type.Literal("Thursday"),
+          Type.Literal("Friday"),
+          Type.Literal("Saturday"),
+          Type.Literal("Sunday"),
+        ]),
+      }),
+    ),
+  ),
+  webMaster: Type.Optional(Type.String()),
   ttl: Type.Optional(Type.Number()),
 });
 
