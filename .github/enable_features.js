816597b180f1e1c9fbb6c58234c570fb9a0be37e
flavio <newfla@hotmail.it>
2024-03-20 17:07:44 +0100

    ci-cd: added enable_features script

---
+++ .github/enable_features.js
@@ -0,0 +1,25 @@
+const os = require('os');
+const fs = require("fs");
+const path = "./packages/app/src-tauri/tauri.conf.json";
+fs.readFile(path, "utf8", (error, data) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  let config = JSON.parse(data);
+
+  const opsys = process.platform;
+  if (opsys == "darwin") {
+    config.build.features = ["metal"];
+  } else if (opsys == "linux") {
+    config.build.features = ["mkl"];
+  }
+  
+  fs.writeFile(path, JSON.stringify(config, null, 2), (error) => {
+    if (error) {
+      console.log('An error has occurred ', error);
+      return;
+    }
+    console.log('Data written successfully to disk');
+  });
+});
