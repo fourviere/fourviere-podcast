c59e9fc117be9ea456946fdf02a8e7db3b072b9a
Mauro Murru <murru7@gmail.com>
2024-03-30 03:12:12 +0100

    feat: season, episode and license (#263)

--- packages/core/lib/schema/podcasting/item.ts
+++ packages/core/lib/schema/podcasting/item.ts
@@ -3,4 +3,24 @@ import ValueSchema from "./value";
 
 export const PodcastItemSchema = Type.Object({
   "podcast:value": Type.Optional(ValueSchema),
+  "podcast:season": Type.Optional(
+    Type.Object({
+      "#text": Type.Number(),
+      "@": Type.Optional(
+        Type.Object({
+          name: Type.Optional(Type.String()),
+        }),
+      ),
+    }),
+  ),
+  "podcast:episode": Type.Optional(
+    Type.Object({
+      "#text": Type.Number(),
+      "@": Type.Optional(
+        Type.Object({
+          display: Type.Optional(Type.String()),
+        }),
+      ),
+    }),
+  ),
 });
